ad2cf32fd9777e0142e145f5571b2eb9cd1e2b6e
Franz Maglanque <fcmaglanque@puregold.com.ph>
2026-04-07 15:49:53 +0800

    removed redirect upon login let layout handle

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -16,6 +16,7 @@ export default function TabsLayout() {
   return (
     <ProtectedRoute>
       <Tabs
+        initialRouteName={isSupplierCashier ? 'cart' : 'index'}
         screenOptions={{
           tabBarActiveTintColor: '#0066cc',
           headerShown: false,
